600be66fb7558a85132b456a3c8b489336db602a
binnotkari-wq <VOTRE_EMAIL>
2026-03-01 04:34:24 +0100

    Auto-sync [2a02-8429-3054-0701-2c71-926b-ccb8-4cb4.rev.sfr.net] : 2026-03-01 04:34:24

--- kiwix-launcher.sh
+++ kiwix-launcher.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 ZIM_DIR="/run/media/benoit/1615eb5d-4346-4106-ba33-dbecf0b75b31/local_cache/Kiwix zims"
-PORT=8080
+PORT=8081
 
 # On tue proprement l'instance précédente si elle existe
 pkill kiwix-serve 2>/dev/null
 
 # Lancement du serveur sur le dossier complet (plus pratique pour naviguer entre ZIMs)
-kiwix-serve --port 8080 "$ZIM_DIR"/*.zim &
+kiwix-serve --port 8081 "$ZIM_DIR"/*.zim &
 
 # Attente très courte (le binaire local est quasi instantané)
 sleep 0.5
 
 # Lancement de Firefox
-firefox "http://localhost:$PORT" &
+# firefox "http://localhost:$PORT" &
+xdg-open "http://localhost:$PORT" &
