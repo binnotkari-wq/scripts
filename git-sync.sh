c64c5e31f58d6f4e494484694856d83ea6338976
binnotkari-wq <VOTRE_EMAIL>
2026-03-14 19:30:03 +0100

    Auto-sync [2a02-8429-3054-0701-cc95-c1c8-8ddd-b717.rev.sfr.net] : 2026-03-14 19:30:03

--- git-sync.sh
+++ git-sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PARENT_DIR="$HOME/Mes-Donnees/Git"
-REPOS=("archives" "home-manager" "info_doc" "user-deploy" "nixos-dotfiles" "nixos-install_script" "atomic-install_script" "scripts" "user-dotfiles")
+REPOS=("archives" "home-manager" "info_doc" "user-deploy" "nixos-dotfiles" "nixos-install_script" "atomic-install_script" "scripts" "user-dotfiles" "install-script")
 HOST=$(hostname)
 
 echo "--- Début de la synchronisation sur [$HOST] : $(date) ---"
